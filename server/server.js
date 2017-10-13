c2ad80ab63508d1812878339545399eb9e20a806
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-13 16:47:20 -0500

    inputs and buttons added

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 var port = 5000;
 
+//express static file serving
 app.use(express.static('server/public'));
 
+
+
+// start up the server
 app.listen(port, function(){
     console.log('listening on port', port);
 });
