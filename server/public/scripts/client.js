f58ca8d30e9af599875d95725868c18063b336f5
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-14 14:19:18 -0500

    calculator is functional

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -26,8 +26,8 @@ function sendOperation(){
    console.log('var operator:', operator);
    // object will be data in POST request
    var mathParts = {
-       x: $('#num1').val(),
-       y: $('#num2').val(),
+       first: $('#num1').val(),
+       second: $('#num2').val(),
        type: operator
    }
    console.log('mathParts object:', mathParts);
@@ -38,9 +38,26 @@ function sendOperation(){
        data: mathParts
    }).done(function(response){
        console.log('response sent from server:', response);
-    //get req function will be called here
+    //make GET request when POST request is finished
+    getResult();
    }).fail(function(message){
        console.log('Error', message);
    })
 }
 
+// make GET request
+function getResult(){
+  $.ajax({
+      method: 'GET',
+      url: '/calculate'
+  }).done(function(response){
+      var calculatedResult = response;
+
+      appendResult(calculatedResult.finalAns);
+  })
+}
+
+// display the result on the DOM
+function appendResult(myResult) {
+ $('#result').text(myResult);
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var port = 5000;
-
+var calculation;
 //express static file serving
 app.use(express.static('server/public'));
 
@@ -9,23 +9,39 @@ app.use(express.static('server/public'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-//store what the client is sending
-var calculationStorage = [];
+//maybe will need to store something here?
 
 //POST route '/calculate'
 app.post('/calculate', function(req, res){
   console.log('req.body:', req.body); //will be mathParts
-  var calculateThis = req.body;
-  // store this req.body (data) 
-  calculationStorage.push(calculateThis);
-  console.log('calculationStorage:', calculationStorage);
-  
+  var mathBundle = req.body;
+  calculation = performCalculation(mathBundle);
+ 
   res.sendStatus(201);
 });
 
+
+function performCalculation(obj){
+  var answer; 
+  if(obj.type === 'add'){ 
+  answer = parseInt(obj.first) + parseInt(obj.second);
+  console.log('answer if "add":', answer);
+  } else if(obj.type === 'subtract'){
+    answer = parseInt(obj.first) - parseInt(obj.second);
+    console.log('answer if "subtract":', answer);
+  } else if(obj.type === 'multiply'){
+    answer = parseInt(obj.first) * parseInt(obj.second);
+    console.log('answer if "multiply":', answer);
+  } else if(obj.type === 'divide'){
+    answer = parseInt(obj.first) / parseInt(obj.second);
+    console.log('answer if "divide":', answer);
+  }
+  return answer;
+}
+
 //GET route
 app.get('/calculate', function(req, res){
-  res.send(calculationStorage);
+  res.send({finalAns: calculation});
 })
 
 // start up the server
