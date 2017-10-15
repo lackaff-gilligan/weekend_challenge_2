87415c1cefd7db29c2c545d4fdebddfd8d80aa58
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-15 07:42:40 -0500

    add number buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,33 +1,61 @@
 $(document).ready(start);
 
 function start(){
-    appendStart();
-    $('.operator').on('click', sendOperation);
-    $('#clear').on('click', restart);
+    //appendBaseStart();
+    appendNumberBtns();
+    //addClickHandlers();
+}
+
+function appendNumberBtns() {
+  for(var i = 0; i < 10; i += 1){
+      $('#numBtns').append('<button id="num' + i + '">' + i + '</button>');
+  }
+
+//   var $opBtns = $('<div class="opBtns"></div>');
+//   $opBtns.append('<button class="operator" id="add">ADD</button>');
+//   $opBtns.append('<button class="operator" id="subtract">SUBTRACT</button>');
+//   $opBtns.append('<button class="operator" id="multiply">MULTIPLY</button>');
+//   $opBtns.append('<button class="operator" id="divide">DIVIDE</button><br>');
+//   $opBtns.append('<button class="operator" id="equals">EQUALS</button><br>');
+//   $opBtns.append('<button id="clear">CLEAR</button>');
+//   //append to the DOM
+//   $('.container').append('<h2 id="display">hey</h2>');
+//   $('.container').append($numBtns);
+//   $('.container').append($opBtns);
+}
+
+function addClickHandlers(){
+    $numBtns.on('click', 'button', useNumber);
+}
+
+function useNumber(){
+    $('#display').append($(this).text());
 }
 
 //create and append inputs and buttons to .container div
-function appendStart(){
-    var $userInput = $('<div class="inputs"></div>');
-    $userInput.append('<label for="num1">First Number: </label><br>');
-    $userInput.append('<input type="number" id="num1"><br>');
-    $userInput.append('<label for="num2">Second Number: </label><br>');
-    $userInput.append('<input type="number" id="num2"><br>');
-    $userInput.append('<button class="operator" id="add">ADD</button>');
-    $userInput.append('<button class="operator" id="subtract">SUBTRACT</button>');
-    $userInput.append('<button class="operator" id="multiply">MULTIPLY</button>');
-    $userInput.append('<button class="operator" id="divide">DIVIDE</button>');
-    $userInput.append('<h2>RESULT: <span id="result"></span></h2>');
-    $userInput.append('<button id="clear">CLEAR</button>');
-    $('.container').append($userInput);
-}
+// function appendBaseStart(){
+//     var $userInput = $('<div class="inputs"></div>');
+//     $userInput.append('<label for="num1">First Number: </label><br>');
+//     $userInput.append('<input type="number" id="num1"><br>');
+//     $userInput.append('<label for="num2">Second Number: </label><br>');
+//     $userInput.append('<input type="number" id="num2"><br>');
+//     $userInput.append('<button class="operator" id="add">ADD</button>');
+//     $userInput.append('<button class="operator" id="subtract">SUBTRACT</button>');
+//     $userInput.append('<button class="operator" id="multiply">MULTIPLY</button>');
+//     $userInput.append('<button class="operator" id="divide">DIVIDE</button>');
+//     $userInput.append('<h2>RESULT: <span id="result"></span></h2>');
+//     $userInput.append('<button id="clear">CLEAR</button>');
+//     $('.container').append($userInput);
+//     //add event handlers
+//     $('.operator').on('click', sendOperation);
+//     $('#clear').on('click', restart);
+// }
 
-// empty .container and then re-appendStart() to the DOM
+// empty .container and then re-appendBaseStart() to the DOM
 function restart(){
     $('.container').empty();
-    appendStart();
-    $('.operator').on('click', sendOperation);
-    $('#clear').on('click', restart);
+    appendBaseStart();
+
 }
 
 function sendOperation(){
